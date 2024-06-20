014375094884ce331e1aec8566282320cf07f4d4
Eric_Lian <git@superexercisebook.com>
2024-06-20 12:50:59 +0800

    feat: add skia

---
+++ src/skia/textlayout/TextStyle.h
@@ -0,0 +1,11 @@
+#ifndef MILESTRO_TEXTSTYLE_H
+#define MILESTRO_TEXTSTYLE_H
+
+#include "modules/skparagraph/include/TextStyle.h"
+
+class TextStyle {
+private:
+    skia::textlayout::TextStyle textStyle;
+};
+
+#endif //MILESTRO_TEXTSTYLE_H
